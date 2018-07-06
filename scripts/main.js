aaba0c5c18559d1e686a21069aa97db7c6d4fe66
Matthew Riach <matthewriach@Matthews-MacBook-Air.local>
2018-07-06 14:58:08 -0600

    made weather show up

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,16 @@
-const url = "https://www.metaweather.com/api/location/2391279"
+const url = "https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/2391279"
 
 fetch(url)
-.then(Response => Response.json())
-.then(data => console.log(data))
+.then(response => response.json())
+.then(data => displayWeather(data.consolidated_weather))
 .catch(console.error)
+
+function displayWeather(weather){
+    var name = document.querySelector('.result')
+    console.log(name)
+    for (let index = 0; index < weather.length; index++) {
+        var item = document.createElement('li')
+        item.textContent = weather[index].weather_state_name
+        name.appendChild(item)
+    }
+}
